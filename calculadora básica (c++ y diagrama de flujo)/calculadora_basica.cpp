c4d8bd6a10434debc2b275b8d2b5d2e9b8dc1c78
Carlos González <ll3224645mail.com>
2022-01-29 15:52:44 -0600

    calculadora

---
+++ calculadora básica (c++ y diagrama de flujo)/calculadora_basica.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero1, numero2;
+    cout << endl <<"Introduce el primer numero: " << endl;
+    cin >> numero1;
+    cout << endl << "Introduce el segundo numero: " << endl;
+    cin >> numero2;
+    cout << endl << "La suma de los dos numeros es: " << numero1+numero2 << endl;
+    cout << endl << "La resta de los dos numeros es: " << numero1-numero2 << endl;
+    cout << endl << "La multiplicacion de los dos numeros es: " << numero1*numero2 << endl;
+    cout << endl << "La dvision de los dos numeros es: " << numero1/numero2 << endl;
+    return 0;
+}
