8f8068a2e223a0263577a691bf44c41e62301818
Carlos González <ll3224645@gmail.com>
2022-01-31 13:25:50 -0600

    adding operations with decimals

--- Tareas primer bimestre (1er. parcial)/calculadora básica (c++ y diagrama de flujo) prueba/calculadora_basica.cpp
+++ Tareas primer bimestre (1er. parcial)/calculadora básica (c++ y diagrama de flujo) prueba/calculadora_basica.cpp
@@ -4,14 +4,31 @@ using namespace std;
 
 int main()
 {
-    int numero1, numero2;
+    float numero1, numero2;
+    string math=" ";
+    cout << endl;
+    cout << "        ==== CALCULADORA BASICA ====" << endl;
     cout << endl <<"Introduce el primer numero: " << endl;
     cin >> numero1;
     cout << endl << "Introduce el segundo numero: " << endl;
     cin >> numero2;
-    cout << endl << "La suma de los dos numeros es: " << numero1+numero2 << endl;
-    cout << endl << "La resta de los dos numeros es: " << numero1-numero2 << endl;
-    cout << endl << "La multiplicacion de los dos numeros es: " << numero1*numero2 << endl;
-    cout << endl << "La dvision de los dos numeros es: " << numero1/numero2 << endl;
+    cout << endl << "        ¿Que operacion desea realizar? (suma, resta, multiplicacion o division)" << endl;
+    cin >> math;
+    if(math == "suma" )
+    {
+        cout << endl << "La suma de los dos numeros es: " << numero1+numero2 << endl;
+    }
+    else if(math == "resta" )
+    {
+        cout << endl << "La resta de los dos números es: " << numero1-numero2 << endl;
+    }
+    else if(math == "multiplicacion" )
+    {
+        cout << endl << "La multiplicacion de los dos numeros es: " << numero1*numero2 << endl;
+    }
+    else if(math == "division")
+    {
+        cout << endl << "La dvision de los dos numeros es: " << numero1/numero2 << endl;
+    }
     return 0;
 }
