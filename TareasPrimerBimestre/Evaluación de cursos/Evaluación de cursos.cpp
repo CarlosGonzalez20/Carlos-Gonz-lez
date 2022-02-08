f0bafc42edb92162b7ce70f92b6e079e000dc3b8
Carlos González <ll3224645@gmail.com>
2022-02-07 19:05:38 -0600

    corrigiendo problemas minimos

--- TareasPrimerBimestre/Evaluación de cursos/Evaluación de cursos.cpp
+++ TareasPrimerBimestre/Evaluación de cursos/Evaluación de cursos.cpp
@@ -35,91 +35,91 @@ int main()
     cout << "=====Evaluando " << cur1 << "=====" << endl;
     if (curso1>=80)
     {
-        cout << "     　Felicidades!!" << endl;
+        cout << "     　Felicidades!! gano con " << curso1 << " puntos" << endl;
     }
     else if (curso1>=61)
     {
-        cout << "     Gano, pero puede mejorar" << endl;
+        cout << "     Gano, pero puede mejorar, con " << curso1 << " puntos" << endl;
     }
     else if (curso1==60)
     {
-        cout << "     Casi lo logra" << endl;
+        cout << "     Casi lo logra, cerca de ganar con " << curso1 << " puntos" << endl;
     }
     else
     {
-        cout << "     Perdio, no llego a la nota minima." << endl;
+        cout << "     Perdio, no llego a la nota minima, con " << curso1 << " puntos" << endl;
     }
     cout << endl;
     cout << "=====Evaluando " << cur2 << "=====" << endl;
     if (curso2>=80)
     {
-        cout << "     　Felicidades!!" << endl;
+        cout << "     　Felicidades!! gano con " << curso2 << " puntos" << endl;
     }
     else if (curso2>=61)
     {
-        cout << "     Gano, pero puede mejorar" << endl;
+        cout << "     Gano, pero puede mejorar, con " << curso2 << " puntos"  << endl;
     }
     else if (curso2==60)
     {
-        cout << "     Casi lo logra" << endl;
+        cout << "     Casi lo logra, cerca de ganar con " << curso2 << " puntos"  << endl;
     }
     else
     {
-        cout << "     Perdio, no llego a la nota minima." << endl;
+        cout << "     Perdio, no llego a la nota, con " << curso2 << " puntos"  << endl;
     }
     cout << endl;
     cout << "=====Evaluando " << cur3 << "=====" << endl;
     if (curso3>=80)
     {
-        cout << "     　Felicidades!!" << endl;
+        cout << "     　Felicidades!! gano con " << curso3 << " puntos"  << endl;
     }
     else if (curso3>=61)
     {
-        cout << "     Gano, pero puede mejorar" << endl;
+        cout << "     Gano, pero puede mejorar, con " << curso3 << " puntos" << endl;
     }
     else if (curso3==60)
     {
-        cout << "     Casi lo logra" << endl;
+        cout << "     Casi lo logra, cerca de ganar con " << curso3 << " puntos" << endl;
     }
     else
     {
-        cout << "     Perdio, no llego a la nota minima." << endl;
+        cout << "     Perdio, no llego a la nota minima, con " << curso3 << " puntos" << endl;
     }
     cout << endl;
     cout << "=====Evaluando " << cur4 << "=====" << endl;
     if (curso4>=80)
     {
-        cout << "     　Felicidades!!" << endl;
+        cout << "     　Felicidades!! gano con " << curso4 << " puntos" << endl;
     }
     else if (curso4>=61)
     {
-        cout << "     Gano, pero puede mejorar" << endl;
+        cout << "     Gano, pero puede mejorar, con " << curso4 << " puntos" << endl;
     }
     else if (curso4==60)
     {
-        cout << "     Casi lo logra" << endl;
+        cout << "     Casi lo logra, cerca de ganar con " << curso4 << " puntos" << endl;
     }
     else
     {
-        cout << "     Perdio, no llego a la nota minima." << endl;
+        cout << "     Perdio, no llego a la nota minima, con " << curso4 << " puntos" << endl;
     }
     cout << endl;
     cout << "=====Evaluando " << cur5 << "=====" << endl;
     if (curso5>=80)
     {
-        cout << "     　Felicidades!!" << endl;
+        cout << "     　Felicidades!! gano con " << curso5 << " puntos" << endl;
     }
     else if (curso5>=61)
     {
-        cout << "     Gano, pero puede mejorar" << endl;
+        cout << "     Gano, pero puede mejorar, con " << curso5 << " puntos" << endl;
     }
     else if (curso5==60)
     {
-        cout << "     Casi lo logra" << endl;
+        cout << "     Casi lo logra, cerca de ganar con " << curso5 << " puntos" << endl;
     }
     else
     {
-        cout << "     Perdio, no llego a la nota minima." << endl;
+        cout << "     Perdio, no llego a la nota minima, con " << curso5 << " puntos" << endl;
     }
     return 0;
 }
